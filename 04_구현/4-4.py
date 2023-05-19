9a3ea492d95829f6bb34e94cc3434ffa67aedf09
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-05-20 01:06:53 +0900

    0520

--- 04_구현/4-4.py
+++ 04_구현/4-4.py
@@ -1,26 +1,22 @@
 from collections import deque
 
-n, m = map(int, input().split())
-x, y, d = map(int, input().split())
-
 # 북 동 남 서
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 
+n, m = map(int, input().split())
+x, y, d = map(int, input().split())
 queue = deque()
 queue.append((x, y))
 
 game_map = []
-
 for i in range(m):
     game_map.append(list(map(int, input().split())))
 
 game_map[y][x] = 1
-
 res = 1
 while queue:
     x, y = queue.pop()
-    print(f">>{x}, {y}")
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
