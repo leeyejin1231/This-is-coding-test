c57e071be3fabb85cb0a4e43e0870f863ccc991b
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-07-21 23:10:49 +0900

    0721

--- 기출/01_그리디/06.py
+++ 기출/01_그리디/06.py
@@ -1,2 +1,37 @@
 #무지의 먹방 라이브
 #프로그래머스
+
+def solution(food_times, k):
+    array = []
+    n = len(food_times)
+    pre = 0
+    time = 0
+    
+    if sum(food_times) <= k:
+        return -1
+    
+    for idx, value in enumerate(food_times, 1):
+        array.append((idx, value))
+    
+    array.sort(key=lambda x:x[1], reverse=True)
+    
+    while True:
+        idx, value = array.pop()
+        time = value - pre
+        if k - time*n > 0:
+            k -= time*n
+            n -= 1
+            pre = value
+        else:
+            array.append((idx, value))
+            break
+    
+    array.sort()
+    
+    return array[k%n][0]
+        
+        
+        
+    
+        
+    
--- 기출/03_DFS,BFS/15.py
+++ 기출/03_DFS,BFS/15.py
@@ -1,3 +1,46 @@
 # 특정 거리의 도시 찾기
 # 백준 실버2
 # https://www.acmicpc.net/problem/18352
+# 가중치 없는 노드는 BFS로
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+
+
+# 시간 초과
+# road_map = []
+# result = []
+# shortest = [1000000] * (n)
+# shortest[x-1] = 0
+
+# for i in range(m):
+#     road_map.append(list(map(int, input().split())))
+
+# def dfs(road_map, head, cnt):
+#     for value in road_map:
+#         if value[0] == head:
+#             if shortest[value[1]-1] > cnt:
+#                 shortest[value[1]-1] = cnt
+#             dfs(road_map, value[1], cnt+1)
+#     return
+            
+# dfs(road_map, x, 1)
+
+# for i, dist in enumerate(shortest, 1):
+#     if dist == k:
+#         result.append(i)
+
+# if len(result) > 0:
+#     for i in sorted(result):
+#         print(i)
+# else:
+#     print(-1)
