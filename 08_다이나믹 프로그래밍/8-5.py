69898a2832e45664e9d3499bcdc2d46645dd28ad
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-08-06 03:31:35 +0900

    0806

--- 08_다이나믹 프로그래밍/8-5.py
+++ 08_다이나믹 프로그래밍/8-5.py
@@ -1,5 +1,19 @@
 # 1로 만들기
+# DP 감이 안와서 답안 참고 함
+# 아니 왜 +1 해주는 거야??? 진짜 이해가 안 돼 ㅜㅠ
+
 x = int(input())
 
-dp = [0] * 3001
+d = [0] * 3001
 
+# botttom up
+for i in range(2, x + 1):
+    d[i] = d[i-1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2]+1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i//5] + 1)
+
+print(d[x])
