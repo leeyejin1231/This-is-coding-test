82467356d4cf0d87b1dc46b54575e2bd5e440e2a
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-08-10 00:23:07 +0900

    0810

--- 09_최단경로/9-5.py
+++ 09_최단경로/9-5.py
@@ -1,1 +1,38 @@
 # 전보
+import sys
+import heapq
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, c = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(c)
+city = -1
+time = 0
+for i in distance:
+    if i != INF:
+        city += 1
+        if time < i:
+            time = i
+
+print(f'{city} {time}')
