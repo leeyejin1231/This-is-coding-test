4bc9034c19e002e84da865a254fde6a29a13d4ac
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-08-18 02:30:24 +0900

    0818

---
+++ 10_그래프 이론/10-1.py
@@ -0,0 +1,37 @@
+# 기본적인 서로소 집합 알고리즘
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+
+for i in range(1, v+1):
+    parent[i] = i
+
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v+1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+print('부모 테이블: ', end='')
+for i in range(1, v+1):
+    print(parent[i], end=' ')
---
+++ 10_그래프 이론/10-5.py
@@ -0,0 +1,4 @@
+# 크루스칼 알고리즘
+
+def find_parent(parent, x):
+    
