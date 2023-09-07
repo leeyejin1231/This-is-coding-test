778167d50a54891804ba66d76a5d42881a38ab54
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-09-08 03:25:44 +0900

    0908

--- 기출/08_그래프이론/42.py
+++ 기출/08_그래프이론/42.py
@@ -1,1 +1,12 @@
 # 탑승구
+g = int(input())
+p = int(input())
+
+graph = [[] for _ in range(p+1)]
+indegree = [0] * (p+1)
+
+for i in range(1, p+1):
+    a = int(input())
+    for j in range(1, a+1):
+        graph[i]
+        indegree[i] += 1
